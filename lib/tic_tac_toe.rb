55adee51c49097d63be4c1064c33f514ef3184e1
Ray Walters <ray.walters@gmail.com>
2014-04-15 01:28:09 +0000

    Start of some new tests and accompanying code.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,2 +1,12 @@
 class TicTacToe
+  DEFAULT_BOARD = [[0,0,0],[0,0,0],[0,0,0]]
+
+  def initialize(board = DEFAULT_BOARD)
+    raise StandardError, "Only square game boards allowed" unless board.length == board.transpose.length
+    @game_board = board.clone
+  end
+
+  def solve
+    return "draw"
+  end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,6 +1,30 @@
 require "tic_tac_toe"
 
 describe TicTacToe do
+  describe ".new" do
+    it "accepts nil for the game board" do
+      expect(TicTacToe.new).to_not be_nil
+    end
+
+    it "accepts a 3x3 two dimensional array" do
+      board = [[0,0,0],[0,0,0],[0,0,0]]
+
+      expect(TicTacToe.new(board)).to_not be_nil
+    end
+
+    it "accepts a 4x4 two dimensional array" do
+      board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+ 
+      expect(TicTacToe.new(board)).to_not be_nil
+    end
+
+    it "raises an error at a 3x4 two dimensional array" do
+      board = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+ 
+      expect {TicTacToe.new(board)}.to raise_error(StandardError, "Only square game boards allowed")
+    end
+  end
+
   describe "#solve" do
     it "shows a draw for an unstarted game" do
       game = TicTacToe.new
