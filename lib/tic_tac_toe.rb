58a7673b731e20fcd57fe6ee186c2165fa2dab81
Ray Walters <ray.walters@gmail.com>
2014-04-16 19:04:21 +0000

    clean up specs and add code to find a certain winners

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,12 +1,56 @@
 class TicTacToe
-  DEFAULT_BOARD = [[0,0,0],[0,0,0],[0,0,0]]
-
-  def initialize(board = DEFAULT_BOARD)
-    raise StandardError, "Only square game boards allowed" unless board.length == board.transpose.length
-    @game_board = board.clone
+  class UnevenBoardError < StandardError
+    def initialize(msg = "Only square game boards allowed")
+      super
+    end
   end
 
-  def solve
-    return "draw"
+  DEFAULT_BOARD = [[0,0,0],[0,0,0],[0,0,0]]
+  PLAYERS = ["", "O", "X"]
+
+  def initialize(board = DEFAULT_BOARD)
+    @game_board = board.clone
+    @transposed_board = transpose(@game_board)
+    raise UnevenBoardError unless @game_board.length == @transposed_board.length
+  end
+
+  def find_winner
+    winner = "draw"
+    winner = find_winner_by_row
+    winner = find_winner_by_column if winner == "draw"
+    return winner
+  end
+
+  private
+  def find_winner_by_row
+    return find_winning_row(@game_board)
+  end
+
+  def find_winner_by_column
+    return find_winning_row(@transposed_board)
+  end
+
+  def find_winning_row(game_board)
+    winner = "draw"
+    game_board.each do |row|
+      initial_cell = row[0]
+      next if initial_cell == 0
+
+      winner = PLAYERS[initial_cell]
+
+      row.each do |cell|
+        winner = "draw" unless cell == initial_cell
+      end
+
+      break unless winner == "draw"
+    end
+
+    return winner
+  end
+
+  def transpose(board)
+    board.transpose
+  rescue IndexError
+    raise UnevenBoardError
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,34 +1,45 @@
 require "tic_tac_toe"
 
 describe TicTacToe do
+  let(:board3x3)        { [[0,1,1],[2,2,2],[0,0,0]] }
+  let(:board4x4)        { [[2,1,2,2],[2,1,0,0],[0,1,0,0],[0,1,0,0]] }
+  let(:board3x4)        { [[0,0,0,0],[0,0,0,0],[0,0,0,0]] }
+  let(:board_staggered) { [[0,0,0],[0,0,0,0],[0,0]] }
+
   describe ".new" do
     it "accepts nil for the game board" do
       expect(TicTacToe.new).to_not be_nil
     end
 
     it "accepts a 3x3 two dimensional array" do
-      board = [[0,0,0],[0,0,0],[0,0,0]]
-
-      expect(TicTacToe.new(board)).to_not be_nil
+      expect(TicTacToe.new(board3x3)).to_not be_nil
     end
 
     it "accepts a 4x4 two dimensional array" do
-      board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
- 
-      expect(TicTacToe.new(board)).to_not be_nil
+      expect(TicTacToe.new(board4x4)).to_not be_nil
     end
 
     it "raises an error at a 3x4 two dimensional array" do
-      board = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
- 
-      expect {TicTacToe.new(board)}.to raise_error(StandardError, "Only square game boards allowed")
+      expect {TicTacToe.new(board3x4)}.to raise_error(TicTacToe::UnevenBoardError, "Only square game boards allowed")
+    end
+
+    it "raises an error at a staggered two dimensional array" do
+      expect {TicTacToe.new(board_staggered)}.to raise_error(TicTacToe::UnevenBoardError, "Only square game boards allowed")
     end
   end
 
-  describe "#solve" do
+  describe "#find_winner" do
     it "shows a draw for an unstarted game" do
       game = TicTacToe.new
-      expect(game.solve).to eq "draw"
+      expect(game.find_winner).to eq "draw"
+    end
+
+    it "shows X as the winner when X wins a row" do
+      expect(TicTacToe.new(board3x3).find_winner).to eq "X"
+    end
+
+    it "shows O as the winner when O wins a column" do
+      expect(TicTacToe.new(board4x4).find_winner).to eq "O"
     end
   end
 end
